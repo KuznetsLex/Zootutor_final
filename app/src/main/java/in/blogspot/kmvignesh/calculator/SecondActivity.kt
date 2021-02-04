3a26a3a6a8b243fa04950dc681ca6bf39ffc2357
Kuznets_lex <kuznets.lex@gmail.com>
2021-02-05 00:19:50 +0600

    First Zootutor commit

---
+++ app/src/main/java/in/blogspot/kmvignesh/calculator/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.tutorialwing.scrollview
+
+
+import `in`.blogspot.kmvignesh.calculator.R
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    //companion object{
+    //    const val TOTAL_COUNT = "total_count"
+    // }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
